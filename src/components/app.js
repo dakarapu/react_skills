3a0f23fcb441e22d9196f78adca9df6646f26de1
dakarapu <ravikanth.dakarapu@gmail.com>
2019-02-23 01:06:20 +1100

    Adding react project

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { component } from 'react';
+
+class APP extends Component {
+    render() {
+        <div>
+            <h1>My React App</h1>
+        </div>
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDom from "react-dom";
+import APP from './components/APP';
+
+ReactDom.render(<App />, document.getElementById("app"))
